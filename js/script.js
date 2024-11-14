98df6686cd819ba750bbd3e005ad18344eee5a9e
Alexsandro Antonio Arantes <132772621+Alexsarantes2023@users.noreply.github.com>
2024-11-14 19:35:21 -0300

    final1

---
+++ js/script.js
@@ -0,0 +1,58 @@
+// Pegar o elemento pelo id
+const bars = document.getElementById("bars");
+const nav = document.getElementById("nav");
+
+// Pegar todos os elementos <a> dentro do menu de navegação
+const navItems = document.querySelectorAll("a");
+
+// Função para abrir ou fechar o menu
+function toggleMenu() {
+  nav.classList.toggle("active"); // Adiciona ou remove a classe "active" do elemento nav
+  bars.classList.toggle("active"); // Adiciona ou remove a classe "active" do elemento bars
+}
+
+// Adiciona um evento de clique no elemento bars para chamar a função toggleMenu
+bars.addEventListener("click", toggleMenu);
+
+// Adiciona um evento de clique a cada item do menu de navegação para chamar a função toggleMenu
+navItems.forEach((item) => {
+  item.addEventListener("click", toggleMenu);
+});
+
+// Cards
+// Pegar o elemento pelo id
+const card = document.getElementById("card");
+const scrollLeft = document.getElementById("left");
+const scrollRight = document.getElementById("right");
+
+// Adiciona um evento de clique ao botão de scroll para a esquerda
+scrollLeft.addEventListener("click", () => {
+  card.scrollBy({
+    left: -200, // Move o elemento card 200 pixels para a esquerda
+    behavior: "smooth", // Adiciona um efeito de rolagem suave
+  });
+  console.log("scrollLeft");
+});
+
+// Adiciona um evento de clique ao botão de scroll para a direita
+scrollRight.addEventListener("click", () => {
+  card.scrollBy({
+    left: 200, // Move o elemento card 200 pixels para a direita
+    behavior: "smooth", // Adiciona um efeito de rolagem suave
+  });
+  console.log("scrollRight");
+});
+
+
+
+//audio toca automatico no site
+//document.addEventListener('DOMContentLoaded', function () { document.getElementById('musica').play(); });
+
+//document.getElementById('musica').addEventListener('canplay', function () {this.play();});
+
+document.addEventListener('DOMContentLoaded', function () {
+  var audio = document.getElementById('musica');
+  audio.load();
+  audio.play();
+});
+
